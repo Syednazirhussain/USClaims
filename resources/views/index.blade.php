4d3984a92183b018a7d6115c466016f7d9e59bf0
Nazir <nazir@golpik.com>
2021-08-25 17:35:05 +0500

    push laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,100 @@
+@extends('layout')
+
+
+@section('content')
+        
+
+    <!-- Start Header Section -->
+    <div class="banner">
+        <div class="overlay">
+            <div class="container">
+                <div class="intro-text">
+                    <h1>Welcome To The Great<span> U.S Claim Services</span></h1>
+                    <p>Generate a flood of new business with the <br> power of a digital media platform</p>
+                    <a href="javascript:void(0);" class="page-scroll btn btn-primary">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Header Section -->
+        
+    <!-- Start Call to Action Section -->
+    <section class="call-to-action">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 wow zoomIn" data-wow-duration="2s" data-wow-delay="300ms">
+                    <p>Awesome Aires Template is ready for <br> Business, Agency, Landing or Creative Portfolio<br>Aires is Responsive and help you to grow your business</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Call to Action Section -->
+    
+    <!-- Start Client Section -->
+    <div id="client-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="client-box">
+                        <ul class="client-list">
+                            <li><a href="javascript:void(0);"><img src="{{ asset('sulfur/asset/images/clients/client1.png') }}" class="img-responsive" alt="Clients Logo"></a></li>
+                            <li><a href="javascript:void(0);"><img src="{{ asset('sulfur/asset/images/clients/client2.png') }}" class="img-responsive" alt="Clients Logo"></a></li>
+                            <li><a href="javascript:void(0);"><img src="{{ asset('sulfur/asset/images/clients/client3.png') }}" class="img-responsive" alt="Clients Logo"></a></li>
+                            <li><a href="javascript:void(0);"><img src="{{ asset('sulfur/asset/images/clients/client4.png') }}" class="img-responsive" alt="Clients Logo"></a></li>
+                            <li><a href="javascript:void(0);"><img src="{{ asset('sulfur/asset/images/clients/client5.png') }}" class="img-responsive" alt="Clients Logo"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Client Section -->
+
+
+    <!-- Start Testimonial Section -->
+    <section id="testimonial-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="testimonial-wrapper">
+                        <div class="testimonial-item">
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+                            <img src="{{ asset('sulfur/asset/images/team/team-1.jpg') }}" alt="Testimonial images">
+                            <h5>John Doe</h5>
+                            <div class="desgnation">CEO, ThemeBean</div>
+                        </div>
+                        <div class="testimonial-item">
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                            <img src="{{ asset('sulfur/asset/images/team/team-2.jpg') }}" alt="Testimonial images">
+                            <h5>John Doe</h5>
+                            <div class="desgnation">CEO, ThemeBean</div>
+                        </div>
+                        <div class="testimonial-item">
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                            <img src="{{ asset('sulfur/asset/images/team/team-3.jpg') }}" alt="Testimonial images">
+                            <h5>John Doe</h5>
+                            <div class="desgnation">CEO, ThemeBean</div>
+                        </div>
+                        <div class="testimonial-item">
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                            <img src="{{ asset('sulfur/asset/images/team/team-4.jpg') }}" alt="Testimonial images">
+                            <h5>John Doe</h5>
+                            <div class="desgnation">CEO, ThemeBean</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Testimonial Section -->
+    
+    
+
+
+@endsection
+    
+
+    
+    
+    
+    
